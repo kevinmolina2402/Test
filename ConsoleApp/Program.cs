d0df4ebd6acc27fc587031051cc92103367ae51f
Kevin <kevinmolina240206@gmail.com>
2025-08-21 09:58:35 -0500

    cambio de .net

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,23 +1,37 @@
 ﻿
-Console.WriteLine("Hello, World")
+Console.WriteLine("Hello, World");
+    Carros carro = new();
+    carro.id =1;
+    carro.placa= "ABC 123";
+    carro.CaballosFuerza = 600;
+    
+    carro.precio = 1000;
+    carro.Encendido = true;
+    carro.FechaFabricacion = DateTime.Now;
+    carro.Marca = new Marcas(){id = 1, Marca = "bmw"};
+    /*marca.modelo = new modelos(){id =1, modelo "M3"}*/;
+
 public class Carros
-{
+{   
+    public int id;
     public string? placa;
     public int CaballosFuerza;
     public float precio;
     public bool Encendido;
     public DateTime FechaFabricacion;
-    public marcas marca;
+    public Marcas? Marca;
 
 
 
 }
 
-public class marcas{
-    public string? marca;
-    public Modelos modelo;
-}
-
-public class modelos{
+public class Marcas{
+    public int id;
+    public string? Marca;
     
 }
+
+/*public class modelos : marcas{
+    public int id;
+    public string? modelo;
+}*/
--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d25c9c96fba769ce3e95cf65c289fbe72a7711b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b17a881fd2e27cf24e4d9c19f5f35dc2ab018833")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
