b17a881fd2e27cf24e4d9c19f5f35dc2ab018833
Kevin <kevinmolina240206@gmail.com>
2025-08-21 09:21:53 -0500

    cambio de .net

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Hello, World")
+public class Carros
+{
+    public string? placa;
+    public int CaballosFuerza;
+    public float precio;
+    public bool Encendido;
+    public DateTime FechaFabricacion;
+    public marcas marca;
+
+
+
+}
+
+public class marcas{
+    public string? marca;
+    public Modelos modelo;
+}
+
+public class modelos{
+    
+}
